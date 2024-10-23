41d7a28c7ab41152435c59c4841f69e62dd72934
bodhi <bodhi.laughton@gmail.com>
2024-10-22 20:51:41 -0500

    Refactored code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JeffBaseTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JeffBaseTeleOpMode.java
@@ -14,13 +14,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 @TeleOp(name = "Jeff Base Two Driver TeleOp", group = "Robot")
 
 public class JeffBaseTeleOpMode extends OpMode {
-    private DcMotor leftSlide;
-    private DcMotor rightSlide;
+    public DcMotor leftSlide;
+    public DcMotor rightSlide;
 
-    private DcMotor leftFrontDrive;
-    private DcMotor leftBackDrive;
-    private DcMotor rightFrontDrive;
-    private DcMotor rightBackDrive;
+    public DcMotor leftFrontDrive;
+    public DcMotor leftBackDrive;
+    public DcMotor rightFrontDrive;
+    public DcMotor rightBackDrive;
     public DcMotor armMotor;
     public CRServo intake;
     public Servo wrist;
@@ -31,18 +31,6 @@ public class JeffBaseTeleOpMode extends OpMode {
                     * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
                     * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
                     * 1 / 360.0; // Ticks per degree, not per rotation
-
-
-    /* These constants hold the position that the arm is commanded to run to.
-    These are relative to where the arm was located when you start the OpMode. So make sure the
-    arm is reset to collapsed inside the robot before you start the program.
-
-    In these variables you'll see a number in degrees, multiplied by the ticks per degree of the arm.
-    This results in the number of encoder ticks the arm needs to move in order to achieve the ideal
-    set position of the arm. For example, the ARM_SCORE_SAMPLE_IN_LOW is set to
-    160 * ARM_TICKS_PER_DEGREE. This asks the arm to move 160° from the starting position.
-    If you'd like it to move further, increase that number. If you'd like it to not move
-    as far from the starting position, decrease it. */
 
     final double ARM_COLLAPSED_INTO_ROBOT = 0;
     final double ARM_COLLECT = 245 * ARM_TICKS_PER_DEGREE;
@@ -66,10 +54,8 @@ public class JeffBaseTeleOpMode extends OpMode {
     final double WRIST_SPECIMEN = 0.4;
     final double WRIST_FOLDED_OUT = 1;
 
-    /* A number in degrees that the triggers can adjust the arm position by */
     final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
 
-    final double BUCKET_DOWN = 1.0;
     final double BUCKET_CATCH = 0.75;
     final double BUCKET_DUMP = 0.3;
 
@@ -103,7 +89,7 @@ public class JeffBaseTeleOpMode extends OpMode {
         rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         armMotor = hardwareMap.get(DcMotor.class, "arm");
-        armMotor.setTargetPosition(0);
+        armMotor.setTargetPosition((int) ARM_COLLAPSED_INTO_ROBOT);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -118,151 +104,14 @@ public class JeffBaseTeleOpMode extends OpMode {
     }
 
     public void loop() {
-        //double left;
-        //double right;
-        //double forward;
-        //double rotate;
-        //if left_trigger: speed = 0.6; else speed = 1.0
-        double speed = gamepad1.right_trigger > 0 ? 0.6 : 1.0;
-        double max;
-
-        if (gamepad2.dpad_up) {
-            leftSlide.setTargetPosition(SLIDE_HIGH);
-            leftSlide.setPower(2.0);
-            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            rightSlide.setTargetPosition(SLIDE_HIGH);
-            rightSlide.setPower(2.0);
-            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        } else if (gamepad2.dpad_down) {
-            leftSlide.setTargetPosition(SLIDE_GROUND);
-            leftSlide.setPower(2.0);
-            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            rightSlide.setTargetPosition(SLIDE_GROUND);
-            rightSlide.setPower(2.0);
-            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        } else if (gamepad2.dpad_left) {
-            leftSlide.setTargetPosition(SLIDE_HALF);
-            leftSlide.setPower(2.0);
-            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            rightSlide.setTargetPosition(SLIDE_HALF);
-            rightSlide.setPower(2.0);
-            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-
-        if (gamepad2.left_trigger > 0) {
-            bucket.setPosition(BUCKET_CATCH);
-        } else if (gamepad2.right_trigger > 0) {
-            bucket.setPosition(BUCKET_DUMP);
-        }
-
-        if (gamepad2.x) {
-            wrist.setPosition(WRIST_FOLDED_OUT);
-        } else if (gamepad2.y) {
-            wrist.setPosition(WRIST_FOLDED_IN);
-        }
-
-        telemetry.addData("Left Linear Slide: ", "%d", leftSlide.getCurrentPosition());
-        telemetry.addData("Right Linear Slide: ", "%d", rightSlide.getCurrentPosition());
-
-        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-        double lateral = gamepad1.left_stick_x;
-        double yaw = gamepad1.right_stick_x;
-
-        // Combine the joystick requests for each axis-motion to determine each wheel's power.
-        // Set up a variable for each drive wheel to save the power level for telemetry.
-        double leftFrontPower = axial + lateral + yaw;
-        double rightFrontPower = axial - lateral - yaw;
-        double leftBackPower = axial - lateral + yaw;
-        double rightBackPower = axial + lateral - yaw;
-
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-
-        if (max > 1.0) {
-            leftFrontPower /= max;
-            rightFrontPower /= max;
-            leftBackPower /= max;
-            rightBackPower /= max;
-        }
-
-        // Send calculated power to wheels
-        leftFrontDrive.setPower(leftFrontPower * speed);
-        rightFrontDrive.setPower(rightFrontPower * speed);
-        leftBackDrive.setPower(leftBackPower * speed);
-        rightBackDrive.setPower(rightBackPower * speed);
-
-        if (gamepad1.a) {
-            intake.setPower(INTAKE_COLLECT);
-        } else if (gamepad1.x) {
-            intake.setPower(INTAKE_OFF);
-        } else if (gamepad1.b) {
-            intake.setPower(INTAKE_DEPOSIT);
-        }
-
-        if (gamepad1.right_bumper) {
-            /* This is the intaking/collecting arm position */
-            armPosition = ARM_COLLECT;
-            wrist.setPosition(WRIST_FOLDED_OUT);
-            intake.setPower(INTAKE_COLLECT);
-        } else if (gamepad1.left_bumper) {
-                    /* This is about 20° up from the collecting position to clear the barrier
-                    Note here that we don't set the wrist position or the intake power when we
-                    select this "mode", this means that the intake and wrist will continue what
-                    they were doing before we clicked left bumper. */
-            wrist.setPosition(WRIST_FOLDED_OUT);
-            armPosition = ARM_CLEAR_BARRIER;
-        } else if (gamepad1.y) {
-            /* This is the correct height to score the sample in the LOW BASKET */
-            armPosition = ARM_SCORE_SAMPLE_IN_LOW;
-        } else if (gamepad1.dpad_left) {
-                    /* This turns off the intake, folds in the wrist, and moves the arm
-                    back to folded inside the robot. This is also the starting configuration */
-            armPosition = ARM_COLLAPSED_INTO_ROBOT;
-            intake.setPower(INTAKE_OFF);
-            wrist.setPosition(WRIST_FOLDED_IN);
-        } else if (gamepad1.dpad_right) {
-            /* This is the correct height to score SPECIMEN on the HIGH CHAMBER */
-            armPosition = ARM_SCORE_SPECIMEN;
-            wrist.setPosition(WRIST_SPECIMEN);
-        } else if (gamepad1.dpad_up) {
-            armPosition = ARM_DEPOSIT;
-            wrist.setPosition(WRIST_FOLDED_IN);
-        } else if (gamepad1.dpad_down) {
-            /* this moves the arm down to lift the robot up once it has been hooked */
-            armPosition = ARM_WINCH_ROBOT;
-            intake.setPower(INTAKE_OFF);
-            wrist.setPosition(WRIST_FOLDED_IN);
-        }
-
-            /* Here we create a "fudge factor" for the arm position.
-            This allows you to adjust (or "fudge") the arm position slightly with the gamepad triggers.
-            We want the left trigger to move the arm up, and right trigger to move the arm down.
-            So we add the right trigger's variable to the inverse of the left trigger. If you pull
-            both triggers an equal amount, they cancel and leave the arm at zero. But if one is larger
-            than the other, it "wins out". This variable is then multiplied by our FUDGE_FACTOR.
-            The FUDGE_FACTOR is the number of degrees that we can adjust the arm by with this function. */
-
-        armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.left_trigger);
-
-        armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
-
-        ((DcMotorEx) armMotor).setVelocity(2100);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
         /* Check to see if our arm is over the current limit, and report via telemetry. */
         if (((DcMotorEx) armMotor).isOverCurrent()) {
             telemetry.addLine("MOTOR EXCEEDED CURRENT LIMIT!");
         }
 
         /* send telemetry to the driver of the arm's current position and target position */
+        telemetry.addData("Left Linear Slide: ", "%d", leftSlide.getCurrentPosition());
+        telemetry.addData("Right Linear Slide: ", "%d", rightSlide.getCurrentPosition());
         telemetry.addData("arm Target: ", armMotor.getTargetPosition());
         telemetry.addData("arm Encoder: ", armMotor.getCurrentPosition());
         telemetry.addData("intake: ", intake.getPower());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
@@ -36,4 +36,120 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 //@Disabled
 public class TwoDriverTeleOpJeff extends JeffBaseTeleOpMode {
+    @Override
+    public void loop () {
+        //if left_trigger: speed = 0.6; else speed = 1.0
+        double speed = gamepad1.right_trigger > 0 ? 0.6 : 1.0;
+        double max;
+
+        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftBackPower = axial - lateral + yaw;
+        double rightBackPower = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send calculated power to wheels
+        leftFrontDrive.setPower(leftFrontPower * speed);
+        rightFrontDrive.setPower(rightFrontPower * speed);
+        leftBackDrive.setPower(leftBackPower * speed);
+        rightBackDrive.setPower(rightBackPower * speed);
+
+        if (gamepad1.a) {
+            intake.setPower(INTAKE_COLLECT);
+        } else if (gamepad1.x) {
+            intake.setPower(INTAKE_OFF);
+        } else if (gamepad1.b) {
+            intake.setPower(INTAKE_DEPOSIT);
+        }
+
+        if (gamepad1.right_bumper) {
+            armPosition = ARM_COLLECT;
+            wrist.setPosition(WRIST_FOLDED_OUT);
+            intake.setPower(INTAKE_COLLECT);
+        } else if (gamepad1.left_bumper) {
+            wrist.setPosition(WRIST_FOLDED_OUT);
+            armPosition = ARM_CLEAR_BARRIER;
+        } else if (gamepad1.y) {
+            armPosition = ARM_SCORE_SAMPLE_IN_LOW;
+        } else if (gamepad1.dpad_left) {
+            armPosition = ARM_COLLAPSED_INTO_ROBOT;
+            intake.setPower(INTAKE_OFF);
+            wrist.setPosition(WRIST_FOLDED_IN);
+        } else if (gamepad1.dpad_right) {
+            armPosition = ARM_SCORE_SPECIMEN;
+            wrist.setPosition(WRIST_SPECIMEN);
+        } else if (gamepad1.dpad_up) {
+            armPosition = ARM_DEPOSIT;
+            wrist.setPosition(WRIST_FOLDED_IN);
+        } else if (gamepad1.dpad_down) {
+            armPosition = ARM_WINCH_ROBOT;
+            intake.setPower(INTAKE_OFF);
+            wrist.setPosition(WRIST_FOLDED_IN);
+        }
+
+        armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.left_trigger);
+
+        armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
+
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        if (gamepad2.dpad_up) {
+            leftSlide.setTargetPosition(SLIDE_HIGH);
+            leftSlide.setPower(2.0);
+            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            rightSlide.setTargetPosition(SLIDE_HIGH);
+            rightSlide.setPower(2.0);
+            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if (gamepad2.dpad_down) {
+            leftSlide.setTargetPosition(SLIDE_GROUND);
+            leftSlide.setPower(2.0);
+            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            rightSlide.setTargetPosition(SLIDE_GROUND);
+            rightSlide.setPower(2.0);
+            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if (gamepad2.dpad_left) {
+            leftSlide.setTargetPosition(SLIDE_HALF);
+            leftSlide.setPower(2.0);
+            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            rightSlide.setTargetPosition(SLIDE_HALF);
+            rightSlide.setPower(2.0);
+            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        if (gamepad2.left_trigger > 0) {
+            bucket.setPosition(BUCKET_CATCH);
+        } else if (gamepad2.right_trigger > 0) {
+            bucket.setPosition(BUCKET_DUMP);
+        }
+
+        if (gamepad2.x) {
+            wrist.setPosition(WRIST_FOLDED_OUT);
+        } else if (gamepad2.y) {
+            wrist.setPosition(WRIST_FOLDED_IN);
+        }
+    }
 }
