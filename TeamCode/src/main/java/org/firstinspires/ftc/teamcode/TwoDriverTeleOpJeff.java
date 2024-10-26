326bd848f278dff56747e06a1cf9db14a8f20b0c
Mike Laughton <32973343+mblaughton@users.noreply.github.com>
2024-10-26 17:05:35 -0500

    Merge f/bodhi/jeff_refactor into jeff (#5)
    * Refactored code
    
    * added stalling feature to prevent linear slide motor overheating
    
    * Added extension limit and slideTargetPosition
    
    * Meet 0 Hotfix
    
    ---------
    
    Co-authored-by: bodhi <bodhi.laughton@gmail.com>
    Co-authored-by: 55Toys <jackyes32123@gmail.com>
    Co-authored-by: ftc-27202 <ftc27202@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
@@ -36,4 +36,133 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 //@Disabled
 public class TwoDriverTeleOpJeff extends JeffBaseTeleOpMode {
+
+    public void loop () {
+        //if left_trigger: speed = 0.6; else speed = 1.0
+        double speed = gamepad1.right_trigger > 0 ? 0.6 : 1.0;
+        double max;
+
+        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftBackPower = axial - lateral + yaw;
+        double rightBackPower = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send calculated power to wheels
+        leftFrontDrive.setPower(leftFrontPower * speed);
+        rightFrontDrive.setPower(rightFrontPower * speed);
+        leftBackDrive.setPower(leftBackPower * speed);
+        rightBackDrive.setPower(rightBackPower * speed);
+
+        if (gamepad1.a) {
+            intake.setPower(INTAKE_COLLECT);
+        } else if (gamepad1.x) {
+            intake.setPower(INTAKE_OFF);
+        } else if (gamepad1.b) {
+            intake.setPower(INTAKE_DEPOSIT);
+        }
+
+        if (gamepad1.right_bumper) {
+            armPosition = ARM_COLLECT;
+            wrist.setPosition(WRIST_FOLDED_OUT);
+            intake.setPower(INTAKE_COLLECT);
+        } else if (gamepad1.left_bumper) {
+            wrist.setPosition(WRIST_FOLDED_OUT);
+            armPosition = ARM_CLEAR_BARRIER;
+        } else if (gamepad1.y) {
+            armPosition = ARM_SCORE_SAMPLE_IN_LOW;
+        } else if (gamepad1.dpad_left) {
+            armPosition = ARM_COLLAPSED_INTO_ROBOT;
+            intake.setPower(INTAKE_OFF);
+            wrist.setPosition(WRIST_FOLDED_IN);
+        } else if (gamepad1.dpad_right) {
+            armPosition = ARM_SCORE_SPECIMEN;
+            wrist.setPosition(WRIST_SPECIMEN);
+        } else if (gamepad1.dpad_up) {
+            armPosition = ARM_DEPOSIT;
+            wrist.setPosition(WRIST_FOLDED_IN);
+        } else if (gamepad1.dpad_down) {
+            armPosition = ARM_WINCH_ROBOT;
+            intake.setPower(INTAKE_OFF);
+            wrist.setPosition(WRIST_FOLDED_IN);
+        }
+
+        armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.left_trigger);
+
+        armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
+
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        if (gamepad2.dpad_up) {
+            slideTargetPosition = SLIDE_HIGH;
+            lastSlideActionTime = getRuntime();
+        } else if (gamepad2.dpad_down) {
+            slideTargetPosition = SLIDE_GROUND;
+            lastSlideActionTime = getRuntime();
+        } else if (gamepad2.dpad_left) {
+            slideTargetPosition = SLIDE_HALF;
+            lastSlideActionTime = getRuntime();
+        }
+
+        if (gamepad2.left_trigger > 0) {
+            bucket.setPosition(BUCKET_CATCH);
+        } else if (gamepad2.right_trigger > 0) {
+            bucket.setPosition(BUCKET_DUMP);
+        }
+
+        if (gamepad2.x) {
+            wrist.setPosition(WRIST_FOLDED_OUT);
+        } else if (gamepad2.y) {
+            wrist.setPosition(WRIST_FOLDED_IN);
+        }
+
+        //slides not in position
+        if (getRuntime() >= lastSlideActionTime + SLIDE_STALL_TIME) {
+           final double leftSlideRemaining = Math.abs(leftSlide.getTargetPosition() - leftSlide.getCurrentPosition());
+           final double rightSlideRemaining = Math.abs(rightSlide.getTargetPosition() - rightSlide.getCurrentPosition());
+
+           if (leftSlideRemaining > 100 || rightSlideRemaining > 100) {
+               leftSlide.setPower(0.0);
+               rightSlide.setPower(0.0);
+               telemetry.addLine("SLIDE(S) STUCK!");
+               return;
+           }
+        }
+
+        //prevents extensions being 42 inches or more
+        if (armMotor.getTargetPosition() > ARM_SCORE_SPECIMEN && (leftSlide.getTargetPosition() > SLIDE_HALF || rightSlide.getTargetPosition() > SLIDE_HALF)) {
+            slideTargetPosition = SLIDE_HALF;
+        }
+
+        leftSlide.setTargetPosition(slideTargetPosition);
+        leftSlide.setPower(2.0);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rightSlide.setTargetPosition(slideTargetPosition);
+        rightSlide.setPower(2.0);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        super.loop();
+    }
 }
