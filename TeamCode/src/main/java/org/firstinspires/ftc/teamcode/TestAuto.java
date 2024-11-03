3db0558cffbc975548cebb7beadb6b13e9218735
ftc-27202 <ftc27202@gmail.com>
2024-11-02 20:21:23 -0500

    changed values for a shorter arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -36,7 +36,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
@@ -45,6 +48,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.Exposur
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -94,24 +98,141 @@ import java.util.concurrent.TimeUnit;
 @Autonomous(name = "Test Auto", group = "drive")
 
 public class TestAuto extends LinearOpMode {
+
+    public DcMotor leftSlide;
+    public DcMotor rightSlide;
+    public DcMotor leftFrontDrive;
+    public DcMotor leftBackDrive;
+    public DcMotor rightFrontDrive;
+    public DcMotor rightBackDrive;
+    public CRServo intake;
+    public Servo wrist;
+    public Servo bucket;
+
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1 / 360.0; // Ticks per degree, not per rotation
+    final double ARM_COLLAPSED_INTO_ROBOT = 0;
+    final double ARM_COLLECT = 235 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER = 245 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_DEPOSIT = 95 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
+
+    final int SLIDE_GROUND = 0;
+    final int SLIDE_HALF = 1350;
+    final int SLIDE_HIGH = 2650;
+    final double SLIDE_STALL_TIME = 2.0;
+
+    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
+    final double INTAKE_COLLECT = -1.0;
+    final double INTAKE_OFF = 0.0;
+    final double INTAKE_DEPOSIT = 0.5;
+
+    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
+    final double WRIST_FOLDED_IN = 0.0;
+    final double WRIST_SPECIMEN = 0.4;
+    final double WRIST_FOLDED_OUT = 1.0;
+
+    final double BUCKET_CATCH = 0.80;
+    final double BUCKET_DUMP = 0.3;
+
+    /* Variables that are used to set the arm to a specific position */
+
+    double armPosition = (int) ARM_COLLAPSED_INTO_ROBOT;
+    double armPositionFudgeFactor;
+    int slideTargetPosition;
+    double lastSlideActionTime = getRuntime();
+
     @Override
     public void runOpMode() {
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         Pose2d startPose = new Pose2d(-24, -60, Math.toRadians(180));
 
         drive.setPoseEstimate(startPose);
 
+
+        leftSlide = hardwareMap.get(DcMotor.class, "leftSlide");
+        leftSlide.setDirection(DcMotor.Direction.FORWARD);
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightSlide = hardwareMap.get(DcMotor.class, "rightSlide");
+        rightSlide.setDirection(DcMotor.Direction.REVERSE);
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftRear");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightRear");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intake = hardwareMap.get(CRServo.class, "intake");
+        intake.setPower(INTAKE_OFF);
+
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        wrist.setPosition(WRIST_FOLDED_IN);
+
+        bucket = hardwareMap.get(Servo.class, "bucket");
+        bucket.setPosition(BUCKET_CATCH);
+        leftSlide.setPower(0.5);
+        rightSlide.setPower(0.5);
+
+        slideTargetPosition = SLIDE_GROUND;
+        leftSlide.setTargetPosition(slideTargetPosition);
+        rightSlide.setTargetPosition(slideTargetPosition);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+            .forward(10)
+            .build();
+
+        drive.followTrajectorySequence(trajSeq);
+
+        //        slideTargetPosition = SLIDE_HIGH;
+//        leftSlide.setTargetPosition(slideTargetPosition);
+//        rightSlide.setTargetPosition(slideTargetPosition);
+//        bucket.setPosition(BUCKET_DUMP);
+        }
+    //}
+}
+
+/*
         waitForStart();
 
         if (isStopRequested()) return;
 
-//        while (!isStopRequested()) {
+       //while (!isStopRequested()) {
         TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                .strafeRight(60)
+                .back(7)
+
+                .addTemporalMarker(12000.0, () -> {
+                    slideTargetPosition = SLIDE_HIGH;
+                    leftSlide.setTargetPosition(slideTargetPosition);
+                    rightSlide.setTargetPosition(slideTargetPosition);
+                    lastSlideActionTime = getRuntime();
+
+                    bucket.setPosition(BUCKET_DUMP);
+                })
                 .build();
         drive.followTrajectorySequence(trajSeq);
-//        }
-    }
-}
+        }
 
+*/
