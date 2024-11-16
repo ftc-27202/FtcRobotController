54da4c3653434348513546cafcf876a5d7551fea
Bodhi Laughton <bodhi.laughton@gmail.com>
2024-11-16 14:58:31 -0600

    cleaned up things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bodhiMachineVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bodhiMachineVision.java
@@ -48,13 +48,11 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 @TeleOp(name = "Bodhi Machine Vision", group = "Robot")
 
-public class bodhiMachineVision extends LinearOpMode
-{
+public class bodhiMachineVision extends LinearOpMode {
     OpenCvCamera phoneCam;
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode()  {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
 
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -124,46 +122,9 @@ public class bodhiMachineVision extends LinearOpMode
         }
     }
 
-    static class SkystoneDeterminationPipeline extends OpenCvPipeline
-    {
+    static class SkystoneDeterminationPipeline extends OpenCvPipeline {
         // An enum to define the skystone position
-        enum SkystonePosition
-        {LEFT, CENTER, RIGHT}
 
-        // Some color constants
-        final Scalar BLUE = new Scalar(0, 0, 255);
-        final Scalar GREEN = new Scalar(0, 255, 0);
-
-        // The core values which define the location and size of the sample regions
-        // The core values which define the location and size of the sample regions
-        final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(50, 130);
-        final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(110, 130);
-        final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(170, 130);
-        static final int REGION_WIDTH = 20;
-        static final int REGION_HEIGHT = 20;
-
-        // Points which actually define the sample region rectangles, derived from above values
-        Point region1_pointA = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x,
-                REGION1_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region2_pointA = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x,
-                REGION2_TOPLEFT_ANCHOR_POINT.y);
-        Point region2_pointB = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region3_pointA = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x,
-                REGION3_TOPLEFT_ANCHOR_POINT.y);
-        Point region3_pointB = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-        // Working variables
-        Mat region1_Cb, region2_Cb, region3_Cb;
         Mat input = new Mat();
         Mat colorR0 = new Mat();
         Mat colorR1 = new Mat();
@@ -174,24 +135,12 @@ public class bodhiMachineVision extends LinearOpMode
         Mat cdst = new Mat();
         Mat hsv = new Mat();
         Mat result = new Mat();
-        int avg1, avg2, avg3;
 
         // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile SkystonePosition position = SkystonePosition.LEFT;
-
-        // This function takes the RGB frame, converts to YCrCb, and extracts the Cb channel to the
-        // 'Cb' variable
-        void inputToCb(Mat input)
-        {
-            //Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            //Core.extractChannel(input, colorR, 0);
-            //Core.extractChannel(input, colorG, 1);
-            //Core.extractChannel(input, colorB, 2);
-        }
+        private volatile int position = 0;
 
         @Override
-        public void init(Mat firstFrame)
-        {
+        public void init(Mat firstFrame) {
             // We need to call this in order to make sure the 'Cb' object is initialized, so that the
             // submats we make will still be linked to it on subsequent frames. (If the object were to
             // only be initialized in processFrame, then the submats would become delinked because the
@@ -216,7 +165,7 @@ public class bodhiMachineVision extends LinearOpMode
             Mat lines = new Mat(); // will hold the results of the detection
             Imgproc.HoughLines(dst, lines, 1, Math.PI/180, 60); // runs the actual detection
             // Draw the lines
-            for (int x = 0; x < lines.rows(); x++) {
+            for (int x = 0; x < Math.min(lines.rows(), 2); x++) {
                 double rho = lines.get(x, 0)[0],
                         theta = lines.get(x, 0)[1];
                 double a = Math.cos(theta), b = Math.sin(theta);
@@ -231,47 +180,11 @@ public class bodhiMachineVision extends LinearOpMode
         @Override
         public Mat processFrame(Mat input)
         {
-            // We first convert to YCrCb color space, from RGB color space.
-            // Why do we do this? Well, in the RGB color space, chroma and
-            // luma are intertwined. In YCrCb, chroma and luma are separated.
-            // YCrCb is a 3-channel color space, just like RGB. YCrCb's 3 channels
-            // are Y, the luma channel (which essentially just a B&W image), the
-            // Cr channel, which records the difference from red, and the Cb channel,
-            // which records the difference from blue. Because chroma and luma are
-            // not related in YCrCb, vision code written to look for certain values
-            // in the Cr/Cb channels will not be severely affected by differing
-            // light intensity, since that difference would most likely just be
-            // reflected in the Y channel.
-            //
-            // After we've converted to YCrCb, we extract just the 2nd channel, the
-            // Cb channel. We do this because stones are bright yellow and contrast
-            // STRONGLY on the Cb channel against everything else, including SkyStones
-            // (because SkyStones have a black label).
-            //
-            // We then take the average pixel value of 3 different regions on that Cb
-            // channel, one positioned over each stone. The brightest of the 3 regions
-            // is where we assume the SkyStone to be, since the normal stones show up
-            // extremely darkly.
-            //
-            // We also draw rectangles on the screen showing where the sample regions
-            // are, as well as drawing a solid rectangle over top the sample region
-            // we believe is on top of the SkyStone.
-            //
-            // In order for this whole process to work correctly, each sample region
-            // should be positioned in the center of each of the first 3 stones, and
-            // be small enough such that only the stone is sampled, and not any of the
-            // surroundings.
-
-            // Get the Cb channel of the input frame after conversion to YCrCb
-            //inputToCb(input);
-            //Mat imgGray = new Mat();
-            //return Imgproc.threshold(colorR,127,255, Imgproc.THRESH_BINARY);
-            //Core.bitwise_not(colorR, colorR);
             Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV, 4);
             
             //red
-            Core.inRange(hsv, new Scalar(240, 25, 170), new Scalar(255, 255, 255), colorR0);
-            Core.inRange(hsv, new Scalar(0, 25, 170), new Scalar(15, 255, 255), colorR1);
+            Core.inRange(hsv, new Scalar(230, 15, 100), new Scalar(255, 255, 255), colorR0);
+            Core.inRange(hsv, new Scalar(0, 15, 170), new Scalar(15, 255, 255), colorR1);
 
             Core.add(colorR0, colorR1, colorR);
             //yellow
@@ -285,22 +198,14 @@ public class bodhiMachineVision extends LinearOpMode
             houghPolar(colorG, new Scalar(255, 255, 0));
             houghPolar(colorB, new Scalar(0, 0, 255));
 
-            //imgThreshold = colorR;
-            //Imgproc.threshold(colorR, imgThreshold, 150, 255, Imgproc.THRESH_BINARY);
-            //Imgproc.adaptiveThreshold(colorR, imgThreshold, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-
-            // Compute the average pixel value of each submat region. We're taking the average of a
-            // single channel buffer, so the value we need is at index 0. We could have also taken the
-            // average pixel value of the 3-channel image, and referenced the value at index 2 here.
-
-            //imgThreshold = (colorR, colorG, colorB);
-            //Core.merge(channels, result);
+            //return result;
+            List<Mat> listMat = Arrays.asList(colorR0, colorG, colorB);
+            Core.merge(listMat, result);
             return result;
-            //return imgThreshold;
         }
 
         // Call this from the OpMode thread to obtain the latest analysis
-        public SkystonePosition getAnalysis()
+        public int getAnalysis()
         {
             return position;
         }
