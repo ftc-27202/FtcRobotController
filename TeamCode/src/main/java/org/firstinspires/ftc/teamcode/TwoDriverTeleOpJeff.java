1566ea859c97ed8ce7904e78cd7aec25555f370e
55Toys <jackyes32123@gmail.com>
2024-11-21 18:44:47 -0600

    thomas's updates for limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
@@ -22,6 +22,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -137,7 +138,19 @@ public class TwoDriverTeleOpJeff extends JeffBaseTeleOpMode {
         } else if (gamepad2.y) {
             wrist.setPosition(WRIST_FOLDED_IN);
         }
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
 
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+        telemetry.update();
         //slides not in position
         if (getRuntime() >= lastSlideActionTime + SLIDE_STALL_TIME) {
            final double leftSlideRemaining = Math.abs(leftSlide.getTargetPosition() - leftSlide.getCurrentPosition());
