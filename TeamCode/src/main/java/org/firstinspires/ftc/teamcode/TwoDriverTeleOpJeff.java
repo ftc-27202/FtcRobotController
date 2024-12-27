64b4dbe61b08250e1d3ed4e66d3aa12640ea1297
55Toys <jackyes32123@gmail.com>
2024-12-26 21:05:29 -0600

    Odometry gets data -offsets not working || -getposx for posy is correct (idk mann)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriverTeleOpJeff.java
@@ -225,6 +225,7 @@ public class TwoDriverTeleOpJeff extends JeffBaseTeleOpMode {
         odo.update();
         telemetry.addData("Pos X","%f", odo.getPosY());
         telemetry.addData("Pos Y","%f", odo.getPosX());
+        telemetry.addData("Position","%f", odo.getPosition());
         //prevents extensions being 42 inches or more
         if (armMotor.getTargetPosition() > ARM_SCORE_SPECIMEN && (leftSlide.getTargetPosition() > SLIDE_HALF || rightSlide.getTargetPosition() > SLIDE_HALF)) {
             slideTargetPosition = SLIDE_HALF;
